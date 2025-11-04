bfdf3a7450cbf7075cd7c96b4c27cd03c29b9715
teusiaqe <jmokatavares@gmail.com>
2025-11-04 14:30:55 -0300

    [dyad] The redundant import of formatCurrency and formatarData from '@/utils/formatters' was removed from PagamentosTab, as these functions are passed via props. - wrote 1 file(s)

--- src/components/contas-pagar/PagamentosTab.tsx
+++ src/components/contas-pagar/PagamentosTab.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
-import { formatCurrency, formatarData } from '@/utils/formatters';
 
 interface PagamentosTabProps {
     loading: boolean;
