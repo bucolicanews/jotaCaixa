1f5f238219473cbc155e4b5ce024cb48e6a6d228
teusiaqe <jmokatavares@gmail.com>
2025-11-10 20:38:19 -0300

    [dyad] Garantido que o cabeçalho da tabela no Plano de Contas permaneça fixo durante a rolagem vertical. - wrote 1 file(s)

--- src/pages/contabilidade/PlanoContas.tsx
+++ src/pages/contabilidade/PlanoContas.tsx
@@ -385,9 +385,9 @@ const PlanoContasPage = () => {
             <CardTitle className="text-xl">Contas Cadastradas ({contas.length})</CardTitle>
           </CardHeader>
           <CardContent>
-            <div className="overflow-x-auto max-h-[60vh]"> {/* Adicionado max-h e overflow-x-auto */}
+            <div className="overflow-x-auto max-h-[60vh] overflow-y-auto"> {/* Adicionado overflow-y-auto */}
               <Table>
-                <TableHeader className="sticky top-0 bg-background z-10"> {/* Adicionado sticky top-0 bg-background z-10 */}
+                <TableHeader className="sticky top-0 bg-background z-10">
                   <TableRow>
                     <TableHead className="w-[150px]">Conta</TableHead>
                     <TableHead className="w-[100px]">Cód. Reduzido</TableHead>
