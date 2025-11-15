132b2d325a44621924dc1d2a070e23de325de38b
teusiaqe <jmokatavares@gmail.com>
2025-11-15 11:00:38 -0300

    Reverted all changes back to version e5f0f77bd04a55ab500871e2f62107acef2213bd

--- src/components/formularios/FormGeral.tsx
+++ src/components/formularios/FormGeral.tsx
@@ -82,7 +82,7 @@ const FormGeral: React.FC<FormGeralProps> = ({
                 <FormLabel className="font-normal">{p.label}</FormLabel>
               </FormItem>
             ))} />
-          ))}
+          )}
         </div>
       </div>
     </div>
