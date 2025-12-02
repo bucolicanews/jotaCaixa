b9fdeed8caea47575563e2454db95cadaf3a0c64
teusiaqe <jmokatavares@gmail.com>
2025-12-02 11:38:54 -0300

    [dyad] wrote 1 file(s)

--- src/components/formularios/FormGeral.tsx
+++ src/components/formularios/FormGeral.tsx
@@ -11,7 +11,7 @@ interface FormGeralProps {
   isSubmitting: boolean;
   permissoesVisiveis: Permissao[];
   handleSelectAll: (select: boolean) => void;
-  isReadOnly: boolean; // FIX: Garantido que isReadOnly está na interface
+  isReadOnly: boolean;
 }
 
 const FormGeral: React.FC<FormGeralProps> = ({
@@ -36,7 +36,7 @@ const FormGeral: React.FC<FormGeralProps> = ({
               {...field} 
               value={field.value === undefined || field.value === null ? '' : String(field.value)}
               onChange={(e) => field.onChange(e.target.value === '' ? undefined : Number(e.target.value))}
-              disabled={disabled || isReadOnly} // Bloqueado se isReadOnly
+              disabled={disabled || isReadOnly}
             />
           </FormControl>
           <FormMessage />
